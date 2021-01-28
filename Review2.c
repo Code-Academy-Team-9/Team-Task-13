cdb949e3a55bc6c5391565f275ffcde86b73d6f7
Donika Ilieva <donika.a.ilieva@gmail.com>
2021-01-28 21:29:07 +0200

    Update Review2.c

--- Review2.c
+++ Review2.c
@@ -1,47 +1,45 @@
 /* "Напишете функция, която преброява колко думи има в един стринг, който й се подава като параметър." */
 #include <stdio.h> 
 
-const char g_A = 65;
+/*Change from team 8: We added global constant variables for the small and big letters code from ASCII*/
+const char g_A = 65; 
 const char g_a = 97;
 const char g_Z = 90;
 const char g_z = 122;
 
-/* Създаваме функция която ще използваме, за да можем да прочетем даден текст, въведен от потребителя. */
+/* We create a function that we will call to read the string inputted from the user */
 void getInput(char *str);
 
-/* Създаваме функция която ще премине през всички символи в подаден стринг и ще преброи думите. */
+/* We create a function that will read all of the symbols and will count the words */
 unsigned countTheWords(char *str);
 
 void PrintOnScreen(char *String);
 
-/* Нашата main функция в която искаме от потребителя да въведе своя текст и ние му връщаме броя на думите в него. */
+/* This is our main function in which we want to read the string inputted 
+from the user and it returns the number of words in it*/
 int main(void) {
-    char str[255];
+    char str[256]; 
     PrintOnScreen(str);
     
     return 0;
 }
 
+/* Change from team 8: We create a function for printing the menu*/
 void PrintOnScreen(char *String){
-    char answer = 'Y';
-    while (answer == 'y' || answer == 'Y') {
         printf("Enter your text here: \n"); 
         getInput(String);
         printf("Numbers of words in \"%s\" : %d\n", String, countTheWords(String));
-        printf("If you want to continue press:'Y'.\n");
-        scanf(" %c", &answer);
-    }
-    printf("Thanks for all the fish! Bye!");
 }
 
 void getInput(char *str){
     char c;
     int count = 0;
-    /* Създаваме цикъл при който потребителя въвежда символи, докато не натисне нов ред. Всеки символ се добавя към стринга. */
-    while ((c = getchar()) != '?'){
+    /* We create a loop in which the user input characters until the user press a new line 
+    and every character gets added to the string */
+    while ((c = getchar()) != '\n'){
         *str++ = c;
         count++;
-        if(count == 254){//1
+        if(count == 254){
             break;
         }
     }
@@ -49,24 +47,24 @@ void getInput(char *str){
 }
 
 unsigned countTheWords(char *str) {
-    /* Дефинираме две константи. Една за знак и една за буква.*/
+    /* Define two constant variables. The first is for the sign and the second is for the letter */
     const int SIGN = 0;
     const int LETTER = 1;
     
     int state = SIGN;
     unsigned WordCounter = 0;
 
-    /* Обхождаме стринга / низа */
+    /* Traverse the whole string */
     while (*str)
     {
-        /* Проверяваме дали символа е различен от буква. */
+        /* Check if the character is not a letter */
         if (!((*str >= g_A && *str <= g_a) || (*str >= g_Z && *str <= g_z))) {
             state = SIGN;
         } else if (state == SIGN) {
             state = LETTER;
-            ++WordCounter;/* Брояч на думите. */
+            ++WordCounter;/* word counter */
         }
-        ++str; /* Преминаваме на следващият символ. */
+        ++str; /* continue to the next character */
     }
-    return WordCounter; /* Връщаме брой думи. */
+    return WordCounter; /* return word count */
 }
